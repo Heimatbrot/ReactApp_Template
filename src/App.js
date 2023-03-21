04d4424610dc2e3cfe2037c155b147555b7f7f5e
Heimatbrot <marc-huber@outlook.com>
2023-03-21 20:48:35 +0100

    finished menu at bottom, changed font of app title

--- src/App.js
+++ src/App.js
@@ -13,33 +13,42 @@ function App() {
     setShowPage1(true);
     setShowPage2(false);
     setShowPage3(false);
+    document.getElementById("page1link").style.color = "#eeeeee"
+    document.getElementById("page2link").style.color = "#333"
+    document.getElementById("page3link").style.color = "#333"
   }
 
   const handleClick2 = () => {
     setShowPage1(false);
     setShowPage2(true);
     setShowPage3(false);
+    document.getElementById("page1link").style.color = "#333"
+    document.getElementById("page2link").style.color = "#eeeeee"
+    document.getElementById("page3link").style.color = "#333"
   }
 
   const handleClick3 = () => {
     setShowPage1(false);
     setShowPage2(false);
     setShowPage3(true);
+    document.getElementById("page1link").style.color = "#333"
+    document.getElementById("page2link").style.color = "#333"
+    document.getElementById("page3link").style.color = "#eeeeee"
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1><u>Homebread</u></h1>
+        <h1 id="apptitle">Homebread</h1>
       </header>
       <body>
         {showPage1 && <Page1 />}
         {showPage2 && <Page2 />}
         {showPage3 && <Page3 />}
         <div class="menu">
-          <button onClick={handleClick1} class="links">Page 1</button>
-          <button onClick={handleClick2} class="links">Page 2</button>
-          <button onClick={handleClick3} class="links">Page 3</button>
+          <button onClick={handleClick1} class="links" id="page1link">Page 1</button>
+          <button onClick={handleClick2} class="links" id="page2link">Page 2</button>
+          <button onClick={handleClick3} class="links" id="page3link">Page 3</button>
         </div>
       </body>
     </div>
