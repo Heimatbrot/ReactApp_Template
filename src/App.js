741a07a080a09341cb85b7c1d8efdceaa51aacf4
Heimatbrot <48zb77j7wg@privaterelay.appleid.com>
2023-03-21 09:55:02 +0100

    added Page 1, 2, 3, Menu and connected those

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import React, { useState } from 'react';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 import './App.css';
 
 function App() {
+  const [showPage1, setShowPage1] = useState(true);
+  const [showPage2, setShowPage2] = useState(false);
+  const [showPage3, setShowPage3] = useState(false);
+
+  const handleClick1 = () => {
+    setShowPage1(true);
+    setShowPage2(false);
+    setShowPage3(false);
+  }
+
+  const handleClick2 = () => {
+    setShowPage1(false);
+    setShowPage2(true);
+    setShowPage3(false);
+  }
+
+  const handleClick3 = () => {
+    setShowPage1(false);
+    setShowPage2(false);
+    setShowPage3(true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Page 1<br></br>
-          <button>Page 2</button>
+          Menu<br></br>
+          <button onClick={handleClick1}>Page 1</button>
+          <button onClick={handleClick2}>Page 2</button>
+          <button onClick={handleClick3}>Page 3</button>
         </p>
+        {showPage1 && <Page1 />}
+        {showPage2 && <Page2 />}
+        {showPage3 && <Page3 />}
       </header>
     </div>
   );
