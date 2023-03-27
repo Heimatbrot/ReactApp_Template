ba2ca8dc61bf0a6dd1cd72c789b1284a4b763a39
Heimatbrot <marc-huber@outlook.com>
2023-03-27 10:03:40 +0200

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
 import React, { useState } from 'react';
-import Page1 from './components/Page1';
+import Page1 from './components/Page1';           // import the pages from components
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
 import './App.css';
 
 function App() {
-  const [showPage1, setShowPage1] = useState(true);
-  const [showPage2, setShowPage2] = useState(false);
-  const [showPage3, setShowPage3] = useState(false);
+  const pages = [
+    { id: 1, component: <Page1 /> },              // add pages, done!
+    { id: 2, component: <Page2 /> },
+    { id: 3, component: <Page3 /> },
+  ];
 
-  const handleClick1 = () => {
-    setShowPage1(true);
-    setShowPage2(false);
-    setShowPage3(false);
-    document.getElementById("page1link").style.color = "#eeeeee"
-    document.getElementById("page2link").style.color = "#333"
-    document.getElementById("page3link").style.color = "#333"
-  }
+  const [activePage, setActivePage] = useState(pages[0].id);
 
-  const handleClick2 = () => {
-    setShowPage1(false);
-    setShowPage2(true);
-    setShowPage3(false);
-    document.getElementById("page1link").style.color = "#333"
-    document.getElementById("page2link").style.color = "#eeeeee"
-    document.getElementById("page3link").style.color = "#333"
-  }
-
-  const handleClick3 = () => {
-    setShowPage1(false);
-    setShowPage2(false);
-    setShowPage3(true);
-    document.getElementById("page1link").style.color = "#333"
-    document.getElementById("page2link").style.color = "#333"
-    document.getElementById("page3link").style.color = "#eeeeee"
-  }
+  const handleClick = (pageId) => {
+    setActivePage(pageId);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 id="apptitle">ReactApp_Template"</h1>
+        <h1 id="apptitle">Homebread</h1>
       </header>
-      <body>
-        {showPage1 && <Page1 />}
-        {showPage2 && <Page2 />}
-        {showPage3 && <Page3 />}
-        <div class="menu">
-          <button onClick={handleClick1} class="links" id="page1link" style={{color: "#eeeeee"}}>Page 1</button>
-          <button onClick={handleClick2} class="links" id="page2link">Page 2</button>
-          <button onClick={handleClick3} class="links" id="page3link">Page 3</button>
-        </div>
-      </body>
+      <nav>
+        {pages.map((page) =>
+          page.id === activePage ? (
+            <div key={page.id}>{page.component}</div>
+          ) : null
+        )}
+      </nav>
+      <div className="menu">
+        {pages.map((page) => (
+          <button
+            onClick={() => handleClick(page.id)}
+            className="links"
+            style={{
+              color: activePage === page.id ? '#eeeeee' : '#333',
+            }}
+          >
+            P{page.id}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
